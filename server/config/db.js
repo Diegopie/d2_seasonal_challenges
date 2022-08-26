2f752f1c64ff592c3f124a26bba64771483877da
Diegopie <ishtaracademy@outlook.com>
2022-08-26 02:24:07 -0600

    pushing starter code

---
+++ server/config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+async function db() {
+    try {
+        mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/d2-challenges', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then(() => {
+            console.log('Successfully Connected To DB');
+        })
+        .catch(err=> {
+            console.log(err);
+        })
+    } catch (error) {
+        console.log('Error Connecting to DB');
+        console.log(error);
+    }
+}
+
+module.exports = db;
