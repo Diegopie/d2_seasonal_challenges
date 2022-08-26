7b6d701f222bfd6ed08ca26cdd13622b0d8ff8fe
Diegopie <ishtaracademy@outlook.com>
2022-08-26 05:43:47 -0600

    added basic responciveness to challenge containers. fixed iframe issue

--- client/src/components/ChallengeCategory/ChallengeCategory.jsx
+++ client/src/components/ChallengeCategory/ChallengeCategory.jsx
@@ -5,45 +5,162 @@ const ChallengeCategory = (props) => {
 
     return (
         <section className='ChallengeCategory-Container'>
-            <article>
+            <article className='ChallengeCategory-Header'>
                 <h2>Week One</h2>
             </article>
-            <section>
-                <article className='Challenge-Header'>
-                    <h3>Challenge Name</h3>
-                </article>
-                <section className='Challenge Body'>
-                    <article>
-                        <p>Challenge description</p>
+            <section className='ChallengeCategory-Body'>
+                <section className='Challenge-Container'>
+                    <article className='Challenge-Header'>
+                        <h3>Challenge Name</h3>
                     </article>
-                    <section>
-                        <article className='ChallengeObjective-Obj'>
-                            <input
-                                id='challenge'
-                                type='checkbox'
-                            />
-                            <p> Treasure Chests unlocked</p>
+                    <section className='Challenge-Body'>
+                        <article>
+                            <p>Challenge description</p>
                         </article>
-                        <article className='ChallengeObjective-Obj'>
-                            <input
-                                id='challenge'
-                                type='checkbox'
-                            />
-                            <p>Plundered Umbral Energy</p>
+                        <section>
                             <article className='ChallengeObjective-Obj'>
-                                <select id='challenge progress' value=''>
-                                    <option disabled>Your Progress</option>
-                                    <option>0</option>
-                                    <option>1</option>
-                                </select>
-                                <p>3</p>
+                                <input
+                                    id='challenge'
+                                    type='checkbox'
+                                />
+                                <p> Treasure Chests unlocked</p>
                             </article>
-                        </article>
+                            <article className='ChallengeObjective-Obj'>
+                                <input
+                                    id='challenge'
+                                    type='checkbox'
+                                />
+                                <p>Plundered Umbral Energy</p>
+                                <article className='ChallengeObjective-Obj'>
+                                    <select id='challenge progress' value=''>
+                                        <option disabled>Your Progress</option>
+                                        <option>0</option>
+                                        <option>1</option>
+                                    </select>
+                                    <p>3</p>
+                                </article>
+                            </article>
+                        </section>
+                        <section>
+                            <h4>Rewards</h4>
+                            <p>Repute</p>
+                            <p>Challenger XP</p>
+                        </section>
                     </section>
-                    <section>
-                        <h4>Rewards</h4>
-                        <p>Repute</p>
-                        <p>Challenger XP</p>
+                </section>
+                <section className='Challenge-Container'>
+                    <article className='Challenge-Header'>
+                        <h3>Challenge Name</h3>
+                    </article>
+                    <section className='Challenge-Body'>
+                        <article>
+                            <p>Challenge description</p>
+                        </article>
+                        <section>
+                            <article className='ChallengeObjective-Obj'>
+                                <input
+                                    id='challenge'
+                                    type='checkbox'
+                                />
+                                <p> Treasure Chests unlocked</p>
+                            </article>
+                            <article className='ChallengeObjective-Obj'>
+                                <input
+                                    id='challenge'
+                                    type='checkbox'
+                                />
+                                <p>Plundered Umbral Energy</p>
+                                <article className='ChallengeObjective-Obj'>
+                                    <select id='challenge progress' value=''>
+                                        <option disabled>Your Progress</option>
+                                        <option>0</option>
+                                        <option>1</option>
+                                    </select>
+                                    <p>3</p>
+                                </article>
+                            </article>
+                        </section>
+                        <section>
+                            <h4>Rewards</h4>
+                            <p>Repute</p>
+                            <p>Challenger XP</p>
+                        </section>
+                    </section>
+                </section><section className='Challenge-Container'>
+                    <article className='Challenge-Header'>
+                        <h3>Challenge Name</h3>
+                    </article>
+                    <section className='Challenge-Body'>
+                        <article>
+                            <p>Challenge description</p>
+                        </article>
+                        <section>
+                            <article className='ChallengeObjective-Obj'>
+                                <input
+                                    id='challenge'
+                                    type='checkbox'
+                                />
+                                <p> Treasure Chests unlocked</p>
+                            </article>
+                            <article className='ChallengeObjective-Obj'>
+                                <input
+                                    id='challenge'
+                                    type='checkbox'
+                                />
+                                <p>Plundered Umbral Energy</p>
+                                <article className='ChallengeObjective-Obj'>
+                                    <select id='challenge progress' value=''>
+                                        <option disabled>Your Progress</option>
+                                        <option>0</option>
+                                        <option>1</option>
+                                    </select>
+                                    <p>3</p>
+                                </article>
+                            </article>
+                        </section>
+                        <section>
+                            <h4>Rewards</h4>
+                            <p>Repute</p>
+                            <p>Challenger XP</p>
+                        </section>
+                    </section>
+                </section><section className='Challenge-Container'>
+                    <article className='Challenge-Header'>
+                        <h3>Challenge Name</h3>
+                    </article>
+                    <section className='Challenge-Body'>
+                        <article>
+                            <p>Challenge description</p>
+                        </article>
+                        <section>
+                            <article className='ChallengeObjective-Obj'>
+                                <input
+                                    id='challenge'
+                                    type='checkbox'
+                                />
+                                <p> Treasure Chests unlocked</p>
+                            </article>
+                            <article className='ChallengeObjective-Obj'>
+                                <input
+                                    id='challenge'
+                                    type='checkbox'
+                                />
+                                <p>Plundered Umbral Energy</p>
+                                <article className='ChallengeObjective-Obj'>
+                                    <select id='challenge progress' value=''>
+                                        <option disabled>Your Progress</option>
+                                        <option>0</option>
+                                        <option>1</option>
+                                    </select>
+                                    <p>3</p>
+                                </article>
+                            </article>
+                        </section>
+                        <section>
+                            <h4>Rewards</h4>
+                            <p>Repute</p>
+                            <p>Challenger XP</p>
+                        </section>
                     </section>
                 </section>
             </section>
