23bdb1a56a75bd452dca70efa6ad51d9567b6734
Diegopie <ishtaracademy@outlook.com>
2022-09-09 04:46:29 -0600

    storing challenge data in local storage; ObjectiveOptions can now find the selected challenge in local storage

--- client/src/components/ChallengeCard/components/ObjectiveOptions/ObjectiveOptions.jsx
+++ client/src/components/ChallengeCard/components/ObjectiveOptions/ObjectiveOptions.jsx
@@ -10,7 +10,10 @@ const ObjectivesSelect = (props) => {
     });
     
     const handleSelect = (e) => {
-        console.log(e.target);
+        const {challenge, index} = e.target.dataset;
+        // successfully getting correct challenge from local storage
+        const test = JSON.parse(localStorage.getItem(challenge));
+        console.log(test.objectives[index]);
         setObjProgress(e.target.value);
     };
 
