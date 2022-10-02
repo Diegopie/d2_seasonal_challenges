0550174e5851e173414b4ad71fe08419936bd887
Diego Hernandez <ishtaracademy@outlook.com>
2022-10-02 16:48:08 -0600

    Commented code

--- client/src/data/parsedSeasonalChallenges.js
+++ client/src/data/parsedSeasonalChallenges.js
@@ -1,46 +1,58 @@
 import seasonalChallenges from "./seasonalChallenges";
 
 const getLocal = (week, challengeIndex, isChallengeComplete, objectiveIndex, isObjectiveComplete) => {
-    // * Find Correct Week
+    // * Find Correct Week and Parse Data
     const dashedWeek = week.replaceAll(' ', '-');
     const getLocal = localStorage.getItem(dashedWeek);
-    const parseLocal = JSON.parse(getLocal)
+    const parseLocal = JSON.parse(getLocal);
+    
     if (isChallengeComplete) {
         //  ** Find Correct Challenge
-        return parseLocal[challengeIndex].completed
+        return parseLocal[challengeIndex].completed;
     }
     if (isObjectiveComplete) {
-        return parseLocal[challengeIndex].objectives[objectiveIndex].completed
+        return parseLocal[challengeIndex].objectives[objectiveIndex].completed;
     }
-    return parseLocal[challengeIndex].objectives[objectiveIndex].progress
+    return parseLocal[challengeIndex].objectives[objectiveIndex].progress;
 }
 
 const localSeasonalChallenges = seasonalChallenges.map((week) => {
+    // * Set weekName to be JSON and LocalStorage Friendly
     const weekName = week.name.replaceAll(' ', '-');
-    // Need to add logic to also update local if a new challenge is added to the week array
+    // * If a Given Week is Not In Local Storage, Save to LocalStorage
     if (localStorage.getItem(weekName) === null) {
         localStorage.setItem(weekName, JSON.stringify(week.challenges));
     }
-    
+    // * If a Challenges are Added to As Week, add Them To LocalStorage without Deleting User Data
     const localData = JSON.parse(localStorage.getItem(weekName))
     const serverData = week.challenges;
+    // ** Checks if Server has new data by comparing the it's length to LocalStorage
     if (serverData.length > localData.length) {
-        console.log("hit");
+        // *** Start Loop with the length of LocalStorage, since we would already have those indexes saved with user data and we do not want to overwrite them with default values
+        //  // Loop through the length of the server data and push any new data at index [i]
         for (let i = localData.length; i < serverData.length; i++) {
-            localData.push(serverData[i])
+            localData.push(serverData[i]);
         }
-        localStorage.setItem(weekName, JSON.stringify(localData))
+        // *** Save Updated Data to Local Storage
+        localStorage.setItem(weekName, JSON.stringify(localData));
     }
     
+    // * For a Given Week, map() Through Each Challenge and Sync Data In Local Storage
     week.challenges.map((challenge ,challengeIndex) => {
+        // ** For a Given Challenge, map() Through Each Objective and Sync Data in Local Storage
         challenge.objectives.map((objective, objectiveIndex) => {
+            // *** Use getLocal to Sync Data
             objective.completed = getLocal(weekName, challengeIndex, false, objectiveIndex, true)
             objective.progress = getLocal(weekName, challengeIndex, false, objectiveIndex, false)
-            return objective
+            // *** Return Updated Data
+            return objective;
         })
-        challenge.completed = getLocal(weekName, challengeIndex, true)
-        return challenge
+        // *** Use getLocal to Sync Data
+        challenge.completed = getLocal(weekName, challengeIndex, true);
+        // *** Return Updated Data
+        return challenge;
     })
+    // *** Return Updated Data
     return week;
 });
 
