53d7267c0515037c038dc7a17860f9d0c8996d2d
Diego Hernandez <ishtaracademy@outlook.com>
2022-12-04 16:24:35 -0700

    updated seasonalChallenges with correct formating

--- client/src/data/seasonalChallenges.js
+++ client/src/data/seasonalChallenges.js
@@ -107,105 +107,113 @@ const seasonalChallenges = [
                 dust: null
             },
         ]
-        // },
+        },
         // Week Two
-        // {
-        //     name: 'Week Two',
-        //     challenges: [
-        //         {
-        //             name: 'Antiquarian II',
-        //             description: 'Return the second relic to the H.E.L.M. Also, defeat Fallen. Defeating them with precision final blows will award bonus progress',
-        //             category: ['Expedition'],
-        //             completed: false,
-        //             objectives: [
-        //                 {
-        //                     isPercent: false,
-        //                     task: "Relic Placed",
-        //                     progress: 0,
-        //                     goal: 2,
-        //                     completed: false,
-        //                 },
-        //                 {
-        //                     isPercent: false,
-        //                     task: "Fallen Killed",
-        //                     progress: 0,
-        //                     goal: 250,
-        //                     completed: false,
-        //                 },
-        //             ],
-        //             reward: 'Repute',
-        //             xp: 'XP',
-        //             dust: null
-        //         },
-        //         {
-        //             name: 'Expert Expedition I',
-        //             description: 'Complete Expeditions and gather treasure in the treasure haul',
-        //             category: ['Expedition'],
-        //             completed: false,
-        //             objectives: [
-        //                 {
-        //                     isPercent: false,
-        //                     task: "Expeditions Completed",
-        //                     progress: 0,
-        //                     goal: 3,
-        //                     completed: false,
-        //                 },
-        //                 {
-        //                     isPercent: false,
-        //                     task: "Treasure Gathered",
-        //                     progress: 0,
-        //                     goal: 25,
-        //                     completed: false,
-        //                 },
-        //             ],
-        //             reward: 'Repute',
-        //             xp: 'XP',
-        //             dust: null
-        //         },
-        //         {
-        //             name: "Seeker's Cache I",
-        //             description: 'Complete bounties from the Star Chart or on Nessus, Cosmodrome, or Europa. Also, decipher the Small Treasure Beacon, available from the Star Chart in the H.E.L.M.',
-        //             category: ['Activities'],
-        //             completed: false,
-        //             objectives: [
-        //                 {
-        //                     isPercent: false,
-        //                     task: 'Bounties',
-        //                     progress: 0,
-        //                     goal: 15,
-        //                     completed: false,
-        //                 },
-        //                 {
-        //                     isPercent: false,
-        //                     task: 'Small Treasure Beacon',
-        //                     progress: 0,
-        //                     goal: 1,
-        //                     completed: false,
-        //                 },
-        //             ],
-        //             reward: null,
-        //             xp: 'XP',
-        //             dust: null
-        //         },
-        //         {
-        //             name: 'Fusillade',
-        //             description: 'Defeat targets with Submachine Guns. Defeating combatants in Ketchcrash and defeating Guardians will award bonus progress',
-        //             category: ['Ketchcrash', 'Crucible'],
-        //             completed: false,
-        //             objectives: [
-        //                 {
-        //                     isPercent: false,
-        //                     task: "Submachine Gun kills",
-        //                     progress: 0,
-        //                     goal: 200,
-        //                     completed: false,
-        //                 }
-        //             ],
-        //             reward: null,
-        //             xp: 'XP',
-        //             dust: null
-        //         },
-        //     ]
+        {
+            name: 'Week Two',
+            challenges: [
+                {
+                    belongsTo: 'Week-Two',
+                    name: 'Antiquarian II',
+                    description: 'Return the second relic to the H.E.L.M. Also, defeat Fallen. Defeating them with precision final blows will award bonus progress',
+                    category: ['Expedition'],
+                    completed: false,
+                    challengeIndex: 0,
+                    objectives: [
+                        {
+                            isPercent: false,
+                            task: "Relic Placed",
+                            progress: 0,
+                            goal: 2,
+                            completed: false,
+                        },
+                        {
+                            isPercent: false,
+                            task: "Fallen Killed",
+                            progress: 0,
+                            goal: 250,
+                            completed: false,
+                        },
+                    ],
+                    reward: 'Repute',
+                    xp: 'XP',
+                    dust: null
+                },
+                {
+                    belongsTo: 'Week-Two',
+                    name: 'Expert Expedition I',
+                    description: 'Complete Expeditions and gather treasure in the treasure haul',
+                    category: ['Expedition'],
+                    completed: false,
+                    challengeIndex: 1,
+                    objectives: [
+                        {
+                            isPercent: false,
+                            task: "Expeditions Completed",
+                            progress: 0,
+                            goal: 3,
+                            completed: false,
+                        },
+                        {
+                            isPercent: false,
+                            task: "Treasure Gathered",
+                            progress: 0,
+                            goal: 25,
+                            completed: false,
+                        },
+                    ],
+                    reward: 'Repute',
+                    xp: 'XP',
+                    dust: null
+                },
+                {
+                    belongsTo: 'Week-Two',
+                    name: "Seeker's Cache I",
+                    description: 'Complete bounties from the Star Chart or on Nessus, Cosmodrome, or Europa. Also, decipher the Small Treasure Beacon, available from the Star Chart in the H.E.L.M.',
+                    category: ['Activities'],
+                    completed: false,
+                    challengeIndex: 2,
+                    objectives: [
+                        {
+                            isPercent: false,
+                            task: 'Bounties',
+                            progress: 0,
+                            goal: 15,
+                            completed: false,
+                        },
+                        {
+                            isPercent: false,
+                            task: 'Small Treasure Beacon',
+                            progress: 0,
+                            goal: 1,
+                            completed: false,
+                        },
+                    ],
+                    reward: null,
+                    xp: 'XP',
+                    dust: null
+                },
+                {
+                    belongsTo: 'Week-Two',
+                    name: 'Fusillade',
+                    description: 'Defeat targets with Submachine Guns. Defeating combatants in Ketchcrash and defeating Guardians will award bonus progress',
+                    category: ['Ketchcrash', 'Crucible'],
+                    completed: false,
+                    challengeIndex: 3,
+                    objectives: [
+                        {
+                            isPercent: false,
+                            task: "Submachine Gun kills",
+                            progress: 0,
+                            goal: 200,
+                            completed: false,
+                        }
+                    ],
+                    reward: null,
+                    xp: 'XP',
+                    dust: null
+                },
+            ]
     },
 ]
 
