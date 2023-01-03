ecbdb50463d2b4d52d108a93c5a76dfab620e04d
Diego Hernandez <ishtaracademy@outlook.com>
2023-01-03 09:50:39 -0700

    added basic bootstrap menue, added react packages to client folder

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import ChallengeCategory from '../components/ChallengeCategory/ChallengeCategory';
 import localSeasonalChallenges from '../data/parsedSeasonalChallenges.js';
+import Menu from '../layouts/Menu';
 
 const Homepage = () => {
 
-    
+
 
 
     return (
-        <main className='App-backMaxHeight'>
-            <section className='App-backImg App-backImg-1-light'>
-                {localSeasonalChallenges.map((week) => {
-                    return (
-                        <ChallengeCategory
-                            key={week.name}
-                            name={week.name}
-                            challenges={week.challenges}
-                        />
-                    )
-                })}
-            </section>
-        </main>
+        <>
+            <Menu />
+            <main className='App-backMaxHeight'>
+                <section className='App-backImg App-backImg-1-light'>
+                    {localSeasonalChallenges.map((week) => {
+                        return (
+                            <ChallengeCategory
+                                key={week.name}
+                                name={week.name}
+                                challenges={week.challenges}
+                            />
+                        )
+                    })}
+                </section>
+            </main>
+        </>
     );
 };
 
