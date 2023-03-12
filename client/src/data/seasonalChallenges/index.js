33fc89c7e554bcb9211c2345599d969126190cee
Diegopie <ishtaracademy@outlook.com>
2023-03-12 00:26:22 -0700

    added week 2 data

--- client/src/data/seasonalChallenges/index.js
+++ client/src/data/seasonalChallenges/index.js
@@ -1,5 +1,5 @@
 import week01 from "./week01";
-// import week02 from "./week02";
+import week02 from "./week02";
 // import week03 from "./week03";
 // import week04 from "./week04";
 // import week05 from "./week05";
@@ -13,7 +13,7 @@ let seasonalChallenges = [];
 
 seasonalChallenges.push(
     week01,
-    // week02,
+    week02,
     // week03,
     // week04,
     // week05,
