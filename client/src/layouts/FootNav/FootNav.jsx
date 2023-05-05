57e649daddfa789a2f96b59ab0ecaa7310f78cf4
Diegopie <ishtaracademy@outlook.com>
2023-05-05 16:07:40 -0600

    added info as a dropUp menu

--- client/src/layouts/FootNav/FootNav.jsx
+++ client/src/layouts/FootNav/FootNav.jsx
@@ -1,21 +1,26 @@
+import { useState } from 'react';
 import './FootNav.css';
-import Coffee from './components/Coffee';
 import DropUpMenu from './components/DropUpMenu';
+import Info from './components/Info';
 
 const FootNav = (props) => {
+
+  const [showInfo, setShowInfo] = useState(false);
 
   if (window.location.pathname === '/') {
     return null;
   }
 
+
   const handleClick = (menu) => {
     const target = document.getElementById(menu);
+    const className = `FootNav-${menu}-ShowContainer`;
 
     if (target.dataset.show === 'false') {
-      target.classList.add('FootNav-DropUp-ShowContainer');
+      target.classList.add(className);
       target.dataset.show = 'true'
     } else {
-      target.classList.remove('FootNav-DropUp-ShowContainer');
+      target.classList.remove(className);
       target.dataset.show = 'false';
     }
   }
@@ -25,13 +30,18 @@ const FootNav = (props) => {
       <article className='FootNav-Container'>
 
         <nav className='FootNav-Body'>
-          <button className='App-Button FootNav-Button' onClick={() => { handleClick('dropUp') }}>Categories</button>
+          <button className='App-Button FootNav-Button' onClick={() => { handleClick('DropUp') }}>Categories</button>
           <button className='App-Button FootNav-Button'><a href='#NavBar'>Top</a></button>
+          <button className='App-Button FootNav-Button' onClick={() => { handleClick('Info') }}>Top</button>
           {/* <button className='App-Button FootNav-Button'>Tracked</button> */}
         </nav>
-        <DropUpMenu 
+        <DropUpMenu
           handleClick={handleClick}
           menu='dropUp'
+        />
+        <Info 
+          showInfo={showInfo}
+          setShowInfo={setShowInfo}
         />
       </article>
     </section>
---
+++ client/src/layouts/FootNav/components/Info.jsx
@@ -0,0 +1,30 @@
+
+const Info = (props) => {
+
+    return (
+        <section className='FootNav-DropUp-Container FootNav-Info-ShowContaine' id='Info' data-show='false'>
+            <article className='FootNav-Info-Body App-TextCenter'>
+                <h2>Hello, Gaurdians!</h2>
+            </article>
+            <article className='FootNav-Info-Body'>
+                <p>Destiny 2 Seasonal Challenges is designed to help you make the most of your time.</p>
+                <p>Visit the <span><a className='App-Link'  href="/activites">Activities</a></span> page to find an activity in the game you want to do. This will show you all the current Seasonal Challenges you can knock out at the same time!</p>
+                <p>Mark challenges as complete to hide them from view and use the togglers at the top of each Activity to bring them back (I aim to make this easier in the <span><a className='App-Link' href='#future'>future</a></span>)</p>
+            </article>
+            <article className='FootNav-Info-Body'>
+                <h2>Feedback</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi natus exercitationem voluptas quia quidem deserunt tenetur, perferendis inventore tempore, minus temporibus fugiat vel eius </p>
+            </article>
+            <article className='FootNav-Info-Body'>
+                <h2>Support</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi natus exercitationem voluptas quia quidem deserunt tenetur, perferendis inventore tempore, minus temporibus fugiat vel eius </p>
+            </article>
+            <article className='FootNav-Info-Body'>
+                    <h2>What Comes Next?</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi natus exercitationem voluptas quia quidem deserunt tenetur, perferendis inventore tempore, minus temporibus fugiat vel eius </p>
+                </article>
+        </section>
+    )
+};
+
+export default Info;
