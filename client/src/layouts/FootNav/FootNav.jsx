2d838db6acd6727db52aa13bab91f596fb053016
Diegopie <ishtaracademy@outlook.com>
2023-05-05 18:23:42 -0600

    fixed App-Link to traditionally style links; refactored handleMenu to close other menues if they are open

--- client/src/layouts/FootNav/FootNav.jsx
+++ client/src/layouts/FootNav/FootNav.jsx
@@ -12,17 +12,40 @@ const FootNav = (props) => {
   }
 
 
+
   const handleClick = (menu) => {
     const target = document.getElementById(menu);
     const className = `FootNav-${menu}-ShowContainer`;
+    const menus = document.getElementsByClassName('FootNav-DropUp-Container');
+    let isSameMenu = false;
+
+    // * Close Any Open Menus
+    for (let i = 0; i < menus.length; i++) {
+      const thisMenu = menus[i]
+      const id = thisMenu.id;
+      let menuState = thisMenu.dataset.show;
+      if (menuState === 'true') {
+        if (id === menu) {
+          console.log(id, menu);
+          isSameMenu = true;
+        }
+        thisMenu.dataset.show = 'false';
+        thisMenu.classList.remove(`FootNav-${id}-ShowContainer`);
+      }
+    }
+
+    if (isSameMenu) {
+      return;
+    }
 
     if (target.dataset.show === 'false') {
       target.classList.add(className);
       target.dataset.show = 'true'
-    } else {
-      target.classList.remove(className);
-      target.dataset.show = 'false';
     }
+    // } else {
+    //   target.classList.remove(className);
+    //   target.dataset.show = 'false';
+    // }
   }
 
   return (
@@ -32,14 +55,14 @@ const FootNav = (props) => {
         <nav className='FootNav-Body'>
           <button className='App-Button FootNav-Button' onClick={() => { handleClick('DropUp') }}>Categories</button>
           <button className='App-Button FootNav-Button'><a href='#NavBar'>Top</a></button>
-          <button className='App-Button FootNav-Button' onClick={() => { handleClick('Info') }}>Top</button>
+          <button className='App-Button FootNav-Button' onClick={() => { handleClick('Info') }}>Info</button>
           {/* <button className='App-Button FootNav-Button'>Tracked</button> */}
         </nav>
         <DropUpMenu
           handleClick={handleClick}
           menu='dropUp'
         />
-        <Info 
+        <Info
           showInfo={showInfo}
           setShowInfo={setShowInfo}
         />
